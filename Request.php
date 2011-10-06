188d0c0389c55c0d5ac6c864eb3e9e4ededf6763
Satoshi Nishimura <nishim314@gmail.com>
2011-10-06 19:43:22 +0900

    Adding features for session and request.

--- Request.php
+++ Request.php
@@ -2,7 +2,69 @@
 
 class Yokaze_Request
 {
-    public function __construct()
+    private $https;
+    private $host;
+    public function __construct($vars = array())
     {
+        if (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on')
+            $this->https = true;
+        else
+            $this->https = false;
+
+        if (isset($_SERVER['HTTP_HOST']))
+            $this->host = $_SERVER['HTTP_HOST'];
+        else
+            $this->host = gethostname();
+
+        foreach ($vars as $k => $v)
+            $this->$k = $v;
+    }
+
+    public function getMethod()
+    {
+        return $_SERVER['REQUEST_METHOD'];
+    }
+    public function isPost()
+    {
+        return strtolower($this->getMethod()) === 'post';
+    }
+    public function initGet()
+    {
+        if (!isset($_GET) || !is_array($_GET))
+            return $this;
+        return $this->initReq($_GET);
+    }
+    public function initPost()
+    {
+        if (!isset($_POST) || !is_array($_POST))
+            return $this;
+        return $this->initReq($_POST);
+    }
+    private function initReq($req)
+    {
+        foreach ($req as $k => $v)
+            $this->$k = $v;
+        return $this;
+    }
+    public function initDto($dto)
+    {
+        $tmp = clone $dto;
+        foreach ($dto as $k => $v){
+            if (isset($this->$k))
+                $dto->$k = $this->$k;
+            else
+                $this->$k = $dto->$k;
+        }
+    }
+
+    public function redirect($fileName)
+    {
+        if ($this->https)
+            $location = 'https://';
+        else
+            $location = 'http://';
+        $location .= $this->host;
+        $location .= preg_replace('|/[^/]+\.php$|', "/$fileName", $_SERVER['SCRIPT_NAME']);
+        header("Location: $location");
     }
 }
---
+++ Session.php
@@ -0,0 +1,192 @@
+<?php
+/**
+ * Simple Session Management Class File
+ *
+ * PHP versions 5
+ *
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ * @copyright 2005-2011 Satoshi Nishimura
+ */
+
+/**
+ * Simple session manager class.
+ *
+ * @author    Satoshi Nishimura <nishim314@gmail.com>
+ */
+class Yokaze_Session
+{
+    /** @var bool */
+    private $isStarted = false;
+    /** @ver int */
+    private $lifetime = 0;
+    /** @var string */
+    private $path = '/';
+    /** @ver string */
+    private $domain;
+
+    public function setSessionName($name)
+    {
+        session_name($name);
+        return $this;
+    }
+
+    public function setSid($sid)
+    {
+        session_id($sid);
+        return $this;
+    }
+
+    public function setLifetime($time)
+    {
+        $this->lifetime = $time;
+        return $this;
+    }
+
+    public function setPath($path)
+    {
+        $this->path = $path;
+        return $this;
+    }
+
+    public function setDomain($domain)
+    {
+        $this->domain = $domain;
+        return $this;
+    }
+
+    public function start()
+    {
+        session_set_cookie_params($this->lifetime, $this->path, $this->domain);
+        session_start();
+        $this->isStarted = true;
+        $this->init();
+    }
+
+    public function end(){
+        $_SESSION = array();
+        if (isset($_COOKIE[session_name()])){
+            setcookie(session_name(), '', time()-4200);
+        }
+
+        session_destroy();
+        $this->isStarted = false;
+    }
+
+    /**
+     * Initialization of session. todo.
+     */
+    public function init(){
+        $this->set('_ip_'  , $_SERVER['REMOTE_ADDR']);
+        if (isset($_SERVER['HTTP_USER_AGENT']))
+            $this->set('_ua_', $_SERVER['HTTP_USER_AGENT']);
+        else
+            $this->set('_ua_', 'undefined');
+        $this->set('_time_', time());
+    }
+
+    /**
+     * Validation by ip address
+     * @return bool
+     * @access public
+     */
+    public function checkIp(){
+        list($first, $second, $third, $forth) = explode('.',$_SERVER['REMOTE_ADDR']);
+        if (!$s_ip = $this->get('_ip_')){ return false; }
+        
+        list($s_first, $s_second, $s_third, $s_forth) = explode('.', $s_ip);
+
+        if ($first != $s_first || $second != $s_second || $third != $s_third){
+            return false;
+        }
+
+        return true;
+    }
+
+    /**
+     * Validation by user agent
+     *
+     * @return bool
+     * @access public
+     */
+    public function checkUa(){
+        if (isset($_SERVER['HTTP_USER_AGENT']))
+            $ua = $_SERVER['HTTP_USER_AGENT'];
+        else
+            $ua = 'undefined';
+
+        return ($this->get('_ua_') ==  $ua);
+    }
+
+    /**
+     * Returns value by name
+     * @param string $name
+     * @return mixed
+     * @access public
+     */
+    function get($name){
+        if (!$this->isStarted)
+            $this->start();
+
+        if (isset($_SESSION[$name])){
+            return $_SESSION[$name];
+        }
+    }
+
+    /**
+     * Sets value
+     * @param string $name
+     * @param mixed $value;
+     * @access public
+     */
+    function set($name, $value){
+        if (!$this->isStarted)
+            $this->start();
+        $_SESSION[$name] = $value;
+    }
+
+    /**
+     * Deletes value by name
+     *
+     * @param string $name
+     * @access public
+     */
+    function remove($name){
+        if (!$this->isStarted)
+            $this->start();
+        $ret = $this->get($name);
+        unset($_SESSION[$name]);
+        return $ret;
+    }
+
+    /**
+     * Alias of session_id
+     *
+     * @return string
+     */
+    public function getSid(){
+        if (!$this->isStarted)
+            $this->start();
+        return $this->getSessionId();
+    }
+
+    /**
+     * Returns session name
+     *
+     * @return string
+     */
+    public function getSessionName(){
+        if (!$this->isStarted)
+            $this->start();
+        return session_name();
+    }
+
+    /**
+     * Changes session id
+     * @return bool
+     */
+    public function changeSessionId(){
+        if (!$this->isStarted)
+            $this->start();
+        return session_regenerate_id(true);
+    }
+}
