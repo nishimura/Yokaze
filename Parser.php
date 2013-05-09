a087f386d580c80d28714303f9990750ee00bdd4
Satoshi Nishimura <nishim314@gmail.com>
2013-05-09 17:57:51 +0900

    backport from laiz-template v1.2.2.

--- Parser.php
+++ Parser.php
@@ -53,42 +53,11 @@ class Yokaze_Parser extends Yokaze_Template
 
         $ret = '';
         $append = null;
-        $loop = '/^loop:([[:alnum:].]+):([[:alnum:]_]+)(:[[:alnum:]]+)?/';
-        $if = '/^if(el)?:([[:alnum:].<>=\(\)$_!|&-]+)/';
+
         for ($i = 0; $i < $length;){
             $char = $val[$i];
 
-            $sub = substr($val, $i);
-            if (!$append && preg_match($loop, $sub, $m)){
-
-                $ite = str_replace('.', '->', $m[1]);
-                if (isset($m[3])){
-                    $v = ltrim($m[3], ':');
-                    $append = "<?php foreach(\$$ite as \$$m[2]=>\$$v): ?> ";
-                }else{
-                    $append = "<?php foreach(\$$ite as \$$m[2]): ?> ";
-                }
-                $parsed = '';
-                $len = strlen($m[0]);
-                $this->tagStack[count($this->tagStack)-1]['php'] = 'endforeach';
-
-            }else if (!$append && preg_match($if, $sub, $m)){
-                if ($m[1]){
-                    $this->tagStack[count($this->tagStack)-1]['php'] = 'else:';
-                }else{
-                    $this->tagStack[count($this->tagStack)-1]['php'] = 'endif';
-                }
-                $v = str_replace('.', '->', $m[2]);
-                $append = "<?php if (\$$v): ?>";
-                $parsed = '';
-                $len = strlen($m[0]);
-
-            }else if (!$append && preg_match('/^else:/', $sub, $m)){
-                $this->tagStack[count($this->tagStack)-1]['php'] = 'endif';
-                $parsed = '';
-                $len = strlen($m[0]);
-
-            }else if ($char === '{'){
+            if ($char === '{'){
                 list($parsed, $len) = $this->parseVal(substr($val, $i));
 
             }else{
@@ -102,39 +71,106 @@ class Yokaze_Parser extends Yokaze_Template
         return array($close . $ret . $close, $length + 2, $append);
     }
 
+    private function pushPhp($phpcode, $before = false)
+    {
+        $name = $before ? 'before' : 'after';
+        $this->tagStack[count($this->tagStack)-1][$name] =
+            '<?php ' . $phpcode . "\n?>\n"
+            . $this->tagStack[count($this->tagStack)-1][$name];
+    }
+
     private function popTag($closeTag)
     {
         $index = count($this->tagStack) - 1;
         for ($i = $index; $i >= 0; $i--){
             $tag = array_pop($this->tagStack);
             if ($tag['tag'] === $closeTag){
-                if ($tag['php'])
-                    return '<?php ' . $tag['php'] . ' ?>';
-                else
-                    return '';
+                return array($tag['before'], $tag['after']);
             }
         }
-        return '';
+        return array('', '');
     }
     private function parseTag($buf)
     {
         if (preg_match('|^</ *([[:alnum:]]+) *>|', $buf, $matches)){
             // close tag
             $tagName = $matches[1];
-            $append = $this->popTag($tagName);
-            return array($matches[0] . $append, strlen($matches[0]));
+            list($before, $after) = $this->popTag($tagName);
+            return array($before . $matches[0] . $after, strlen($matches[0]));
         }
         if (!preg_match('/^< *([[:alnum:]]+)/', $buf, $matches))
             return array($buf[0], 1);
 
         $tagName = $matches[1];
-        $this->tagStack[] = array('tag' => $tagName, 'php' => null);
+        $this->tagStack[] = array('tag' => $tagName,
+                                  'before' => '', 'after' => '');
 
         $length = strlen($buf);
         $ret = '';
+
+        $loop = '/^tmpl:loop="([[:alnum:]._]+):([[:alnum:]_]+)(:[[:alnum:]_]+)?"/';
+        $if = '/^tmpl:if(el)?="([[:alnum:]._]+)"/';
+
+        $php = '';
+        $form = array('parse' => false,
+                      'tmpl:form' => null,
+                      'type' => '',
+                      'name' => '',
+                      'value' => '');
         for ($i = 0; $i < $length;){
             $char = $buf[$i];
-            if ($char === '"' || $char === "'"){
+
+            $sub = substr($buf, $i);
+            if (preg_match($loop, $sub, $m)){
+
+                $ite = str_replace('.', '->', $m[1]);
+                if (isset($m[3])){
+                    $v = ltrim($m[3], ':');
+                    $php .= "<?php foreach(\$$ite as \$$m[2]=>\$$v): ?>";
+                }else{
+                    $php .= "<?php foreach(\$$ite as \$$m[2]): ?>";
+                }
+                $len = strlen($m[0]);
+                $this->pushPhp('endforeach;');
+                $parsed = '';
+
+            }else if (preg_match($if, $sub, $m)){
+                if ($m[1]){
+                    $this->pushPhp('else:');
+                }else{
+                    $this->pushPhp('endif;');
+                }
+                $v = str_replace('.', '->', $m[2]);
+                $php .= "<?php if (isset(\$$v) && \$$v): ?>";
+                $len = strlen($m[0]);
+                $parsed = '';
+
+            }else if ($this->startsWith($sub, 'tmpl:else')){
+                $this->pushPhp('endif;');
+                $len = strlen('tmpl:else');
+                $parsed = '';
+
+            }else if (preg_match('/^tmpl:form(="[[:alnum:]\.:_]+")?/', $sub, $m)){
+                $form['parse'] = true;
+                $len = strlen($m[0]);
+                if (strtolower($tagName) === 'select')
+                    $form['type'] = 'select';
+                if (isset($m[1]))
+                    $form['tmpl:form'] = trim($m[1], '="');
+                $parsed = '';
+
+            }else if (preg_match('/^(type=|name=|value=)/', $sub, $m)){
+                $ret .= $m[1];
+                $i += strlen($m[1]);
+                $char = $buf[$i];
+
+                list($parsed, $len, $append) =
+                    $this->parseAttr($tagName, $char, substr($buf, $i));
+                if ($append)
+                    $ret = $append . $ret;
+                $form[trim($m[1], '=')] = trim($parsed, $char);
+
+            }else if ($char === '"' || $char === "'"){
                 list($parsed, $len, $append) =
                     $this->parseAttr($tagName, $char, substr($buf, $i));
                 if ($append)
@@ -142,6 +178,14 @@ class Yokaze_Parser extends Yokaze_Template
 
             }else if ($char === '{'){
                 list($parsed, $len) = $this->parseVal(substr($buf, $i));
+
+            }else if ($this->startsWith($sub, '/>')){
+                $ret .= '/>';
+                $i += 2;
+
+                list($before, $after) = $this->popTag($tagName);
+                $ret = $before . $ret . $after;
+                break;
 
             }else if ($char === '>'){
                 $ret .= $char;
@@ -154,11 +198,74 @@ class Yokaze_Parser extends Yokaze_Template
             $ret .= $parsed;
             $i += $len;
         }
-        return array($ret, $i);
+
+        if ($form['parse'])
+            $ret = $this->parseForm($ret, $form);
+
+        return array($php . $ret, $i);
     }
+    private function parseForm($tagStr, $form)
+    {
+        if (!isset($form['value']))
+            return $tagStr;
+        if ($this->startsWith($form['value'], '<?php'))
+            return $tagStr;
+
+        if (substr($tagStr, -2) === '/>'){
+            $ret = substr($tagStr, 0, strlen($tagStr) - 2);
+            $close = '/>';
+        }else{
+            $ret = substr($tagStr, 0, strlen($tagStr) - 1);
+            $close = '>';
+        }
+        $value = '';
+
+        switch ($form['type']){
+        case 'checkbox':
+        case 'radio':
+            $val = $this->nameToValue($form['name']);
+            $value = "<?php if(isset($val) && ($val === true || $val == '$form[value]')) echo ' checked=\"checked\"';?>";
+            break;
+
+        case 'select':
+            if (!isset($form['tmpl:form']))
+                break;
+            $name = '$' . str_replace('.', '->', $form['tmpl:form']);
+            $keyStr = '$__yokazeTemplateKey__';
+            $valStr = '$__yokazeTemplateValue__';
+            if (strpos($name, ':') !== false){
+                list($a, $key, $value) = explode(':', $name);
+                $name = $a;
+                $keyStr = "$valStr->$key";
+                $valStr .= "->$value";
+            }
+
+            $val = $this->nameToValue($form['name']);
+            $php = "if(isset($name) && (is_array($name) || $name instanceof \\Traversable)){ foreach($name as \$__yokazeTemplateKey__ => \$__yokazeTemplateValue__){"
+                . 'echo "<option value=\"";'
+                . "echo $keyStr . '\"';"
+                . "if ($keyStr == $val)"
+                . '  echo " selected=\"selected\"";'
+                . "echo \">$valStr</option>\"; }}";
+
+            $this->pushPhp($php, true);
+            break;
+
+        default:
+            break;
+        }
+        return $ret . $value . $close;
+    }
+    private function nameToValue($name)
+    {
+        $name = str_replace('[', '->', $name);
+        $name = str_replace(']', '', $name);
+        return '$' . $name;
+    }
+
     private function parseVal($buf)
     {
-        if (!preg_match('/^{([[:alnum:]\.:_]+)}/', $buf, $matches))
+        if (!preg_match('|^{([[:alnum:]\.:/_]+)}|', $buf, $matches))
             return array($buf[0], 1);
 
         $name = $matches[1];
@@ -203,14 +310,15 @@ class Yokaze_Parser extends Yokaze_Template
         $ret = '<?php echo ' .  $val . '; ?>';
         return array($ret, $len);
     }
-    protected function compile($tmplFile, $cacheFile)
+    protected function compileInternal($tmplFile, $cacheFile)
     {
-        if (file_exists($cacheFile) && filemtime($tmplFile) <= filemtime($cacheFile)){
-            return;
-        }
-
         $tmpl = file_get_contents($tmplFile);
         $tmpl = $this->parse($tmpl);
-        file_put_contents($cacheFile, $tmpl);
+        return $tmpl;
+    }
+
+    private function startsWith($haystack, $needle)
+    {
+        return strpos($haystack, $needle, 0) === 0;
     }
 }
--- Template.php
+++ Template.php
@@ -12,7 +12,11 @@ class Yokaze_Template
         if ($templateDir)
             $this->templateDir = $templateDir;
         if ($cacheDir)
-            $this->cacheDir;
+            $this->cacheDir = $cacheDir;
+
+        if (!is_writeable($this->cacheDir))
+            throw new RuntimeException("$this->cacheDir directory is not writeable.");
+
         $this->vars = new StdClass();
     }
     public function setExtension($ext)
@@ -23,13 +27,15 @@ class Yokaze_Template
     {
         $this->vars = $vars;
     }
+
     public function setFile($file)
     {
         $this->file = $file;
+        return $this;
     }
     public function getFile()
     {
-        if ($this->file)
+        if ($this->file !== null)
             return $this->file;
         return basename($_SERVER['SCRIPT_FILENAME'], '.php') . '.' . $this->ext;
     }
@@ -41,16 +47,54 @@ class Yokaze_Template
         $tmplFile = $this->templateDir . '/' . $file;
         $cacheFile = $this->cacheDir . '/' . $file;
 
-        $this->compile($tmplFile, $cacheFile);
+        if (!$this->compile($tmplFile, $cacheFile))
+            return;
 
         $this->showCache($cacheFile, $vars);
     }
+
+    public function get($vars = null)
+    {
+        ob_start();
+        $this->show($vars);
+        return ob_get_clean();
+    }
+
+    protected function show404()
+    {
+        header($_SERVER['SERVER_PROTOCOL']. " 404 Not Found");
+
+        echo <<<END
+            <!DOCTYPE HTML PUBLIC "-//IETF//DTD HTML 2.0//EN">
+            <html><head>
+            <title>404 Not Found</title>
+            </head><body>
+            <h1>Not Found</h1>
+            <p>The requested URL was not found on this server.</p>
+            <hr>
+            <address>Yokaze-template</address>
+END;
+    }
     protected function compile($tmplFile, $cacheFile)
     {
-        if (file_exists($cacheFile) && filemtime($tmplFile) <= filemtime($cacheFile)){
-            return;
+        if (!file_exists($tmplFile)){
+            if (file_exists($cacheFile))
+                unlink($cacheFile);
+            $this->show404();
+            return false;
         }
 
+        if (file_exists($cacheFile) && filemtime($tmplFile) <= filemtime($cacheFile)){
+            return true;
+        }
+
+        $tmpl = $this->compileInternal($tmplFile, $cacheFile);
+
+        $this->file_force_contents($cacheFile, $tmpl);
+        return true;
+    }
+    protected function compileInternal($tmplFile, $cacheFile)
+    {
         $tmpl = file_get_contents($tmplFile);
 
         // include feature
@@ -70,7 +114,19 @@ class Yokaze_Template
         $tmpl = preg_replace('/\{([[:alnum:]_>-]*):b\}/', '<?php echo nl2br(htmlspecialchars($$1)); ?>', $tmpl);
         $tmpl = preg_replace('/\{([[:alnum:]_>-]*)\}/', '<?php echo htmlspecialchars($$1); ?>', $tmpl);
 
-        file_put_contents($cacheFile, $tmpl);
+        return $tmpl;
+    }
+
+    // http://php.net/function.file-put-contents.php#84180
+    protected function file_force_contents($path, $contents,
+                                           $flag = 0, $context = null)
+    {
+        $parts = explode('/', $path);
+        $file = array_pop($parts);
+        $dir = '.';
+        foreach($parts as $part)
+            if(!is_dir($dir .= "/$part")) mkdir($dir);
+        return file_put_contents("$dir/$file", $contents, $flag, $context);
     }
     private function showCache($__cacheFile__, $__vars__)
     {
